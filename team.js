fb2d8a327a25d3195c40c56bdf3715fc309446a3
jshvaron <jshva@icloud.com>
2023-02-25 21:00:56 -0600

    Function created: When manager is selected as an employee to be added, a second prompt will populate to intake managers name, id, email, and office number

--- team.js
+++ team.js
@@ -5,21 +5,60 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+    function generateTeam(){
+        inquirer.prompt([
+            {
+                type:'list',
+                name: 'addEmployee',
+                message: 'What type of employee needs to be added?',
+                choices: ['Manager','Engineer', 'Intern', 'Build your Dream Team' ]
+            }
+        ]).then(answers => {
+            switch(answers.addEmployee){
+                case 'Manager':
+                addManager();
+                break;
+            }
 
+            function addManager(){
+                console.log('manager on duty');
+                
+                inquirer.prompt([
+                    {
+                        type:'input',
+                        name: 'mngrName',
+                        message: "Enter the manager's name."
+                    },
+                    {
+                        type:'input',
+                        name: 'mngrId',
+                        message: "Enter the managers's ID."
+                    },
+                    {
+                        type:'input',
+                        name: 'mngrEmail',
+                        message: "Enter the manager's Email."
+                    },
+                    {
+                        type:'input',
+                        name: 'mngrOfficeNum',
+                        message: "Enter the manager's office number."
+                    }
+                ]).then(answers => {
+                console.log(answers);
+                });
+            };
+        });    
+    }
+    generateTeam();
+       
+    
 
-    inquirer.prompt([
-        {
-            type:'checkbox',
-            name: 'EmployeeType',
-            message: 'What type of employee needs to be added?',
-            choices: ['Manager','Engineer', 'Intern', 'Build your Dream Team' ]
-        }
-    ]).then(answers => {
-        console.log('works');
-    });
-
-
-
+        
+    
+    
+    
+ 
 
 
 
