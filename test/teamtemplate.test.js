0ea8a5b471f9be833146714b838d49fc3673c2e6
jshvaron <jshva@icloud.com>
2023-03-12 22:51:56 -0500

    unit test added to see if teamGen generates markup for managers

--- test/teamtemplate.test.js
+++ test/teamtemplate.test.js
@@ -6,3 +6,15 @@ test('teamGen returns a string', () => {
     const result = teamGen(team);
     expect(typeof result).toBe('string');
   });
+
+
+test('teamGen generates markup for managers', () => {
+const team = [
+    { getRole: () => 'Manager', getName: () => 'John', getId: () => '1', getEmail: () => 'john@example.com', getOfficeNumber: () => '1011011001' },
+    { getRole: () => 'Manager', getName: () => 'Jane', getId: () => '2', getEmail: () => 'jane@example.com', getOfficeNumber: () => '1021021022' },
+];
+const result = teamGen(team);
+expect(result).toContain('<h3><i class="fa-solid fa-mug-hot"></i> Manager</h3>');
+expect(result).toContain('<p class="detail-spacer"> Office Number:101</p>');
+expect(result).toContain('<p class="detail-spacer"> Office Number:102</p>');
+});
