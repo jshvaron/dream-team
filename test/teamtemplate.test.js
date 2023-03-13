a17bff6bf4c83974b79c37ab3589a129f6155985
jshvaron <jshva@icloud.com>
2023-03-12 22:56:52 -0500

    replicated markup test for engineers

--- test/teamtemplate.test.js
+++ test/teamtemplate.test.js
@@ -1,13 +1,13 @@
 const teamGen = require('dream-team/teamtemplate.js')
 
-
+//test a return of teamGen string
 test('teamGen returns a string', () => {
     const team = [/* an array of employees */];
     const result = teamGen(team);
     expect(typeof result).toBe('string');
   });
 
-
+//test teamGen array for manager markup
 test('teamGen generates markup for managers', () => {
 const team = [
     { getRole: () => 'Manager', getName: () => 'John', getId: () => '1', getEmail: () => 'john@example.com', getOfficeNumber: () => '1011011001' },
@@ -18,3 +18,15 @@ expect(result).toContain('<h3><i class="fa-solid fa-mug-hot"></i> Manager</h3>')
 expect(result).toContain('<p class="detail-spacer"> Office Number:1011011001</p>');
 expect(result).toContain('<p class="detail-spacer"> Office Number:1021021022</p>');
 });
+
+//test teamGen array for engineer markup
+test('teamGen generates markup for engineers', () => {
+    const team = [
+      { getRole: () => 'Engineer', getName: () => 'John', getId: () => '1', getEmail: () => 'john@example.com', getGithub: () => 'johndoe' },
+      { getRole: () => 'Engineer', getName: () => 'Jane', getId: () => '2', getEmail: () => 'jane@example.com', getGithub: () => 'janedoe' },
+    ];
+    const result = teamGen(team);
+    expect(result).toContain('<h3><i class="fa-solid fa-mug-hot"></i> Engineer</h3>');
+    expect(result).toContain('<p class="detail-spacer"> Github:johndoe</p>');
+    expect(result).toContain('<p class="detail-spacer"> Github:janedoe</p>');
+  });
